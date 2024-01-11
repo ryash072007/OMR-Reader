93223ce599ec11be879e5b1a302830d217d21934
Yash Raj <ryash072007@gmail.com>
2024-01-11 23:15:55 +0400

    omr scan 4 point GOOD

--- omr-scan-4point.py
+++ omr-scan-4point.py
@@ -2,8 +2,11 @@ import cv2
 import numpy as np
 import math
 
+TRANSF_SIZE = 600
+
 def calculate_distance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
 
 # Function to filter tuples based on distance
 def filter_tuples(points, max_distance=5):
@@ -22,7 +25,7 @@ def filter_tuples(points, max_distance=5):
     return final_result
 
 # Load target image and template
-target_image = cv2.imread('images/omr-filled-scan.jpg')
+target_image = cv2.imread('images/omr_sheet.png')
 template = cv2.imread('images/circle.png')
 
 # Convert to grayscale
@@ -39,13 +42,25 @@ loc = np.where(res >= threshold)
 # Draw rectangles around matches
 points = list(zip(*loc[::-1]))
 points = filter_tuples(points, 5)
-print(points)
 
 for pt in points:
-    # print(pt)
     cv2.rectangle(target_image, pt, (pt[0] + template.shape[1], pt[1] + template.shape[0]), (0, 100, 0), 2)
 
+points = sorted(points)
+print(points)
+x = [x[0] for x in points]
+y = [y[1] for y in points]
+
+
+pts1 = np.float32([(54, 312), (508, 312), (56, 724), (506, 724)])
+pts2 = np.float32([[0, 0], [600, 0], [0, 800], [600, 800]])
+
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+result = cv2.warpPerspective(target_image, matrix, (600, 800))
+result = result[template.shape[1] * 2:, :]
+cv2.imwrite("images/test.png", result)
+
 # Display the result
-cv2.imshow('Result', target_image)
+cv2.imshow('Result', result)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
