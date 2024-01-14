6241a48f76412673c3a7c7051e6b10973df8443a
Yash Raj <ryash072007@gmail.com>
2024-01-14 22:31:59 +0400

    gave up

--- scripts/omr-reader.py
+++ scripts/omr-reader.py
@@ -50,7 +50,7 @@ def get_omr_area(thresholded, contours):
     return omr_area
 
 
-def split_image_times(image, h_times, v_times):
+def split_image_times_row(image, h_times, v_times):
     # Calculate the split dimensions
     h_size = int(image.shape[0] / h_times)
     v_size = int(image.shape[1] / v_times)
@@ -64,6 +64,30 @@ def split_image_times(image, h_times, v_times):
         images.append(row)
 
     return images
+
+
+def split_image_times_list(image, h_times, v_times):
+    # Calculate the split dimensions
+    h_size = int(image.shape[0] / h_times)
+    v_size = int(image.shape[1] / v_times)
+    # Split the image
+    images = []
+    for i in range(0, image.shape[0], h_size):
+        for j in range(0, image.shape[1], v_size):
+            images.append(image[i : i + h_size, j : j + v_size])
+
+    return images
+
+
+def split_image_percentage(image, proportion):
+    # Calculate the split position
+    split_position = int(image.shape[0] * proportion)
+
+    # Split the image
+    image1 = image[:split_position, :]
+    image2 = image[split_position:, :]
+
+    return image1, image2
 
 
 def get_areas_in_omr(omr_area, contours):
@@ -92,7 +116,6 @@ def read_omr(image_path):
 
     # Process each contour and extract relevant information
     omr_area = get_omr_area(thresholded, image_contours)
-    # cv2.imwrite("split/omr_area.jpg", omr_area)
 
     #
     # Get contours and hierarchy
@@ -100,16 +123,49 @@ def read_omr(image_path):
         omr_area, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE
     )
 
+    _, question_area = split_image_percentage(omr_area, 1 - 0.955)
+    part_1, part_2, part_3 = split_image_times_list(question_area, 1, 3)
+
+    part_1_parts = split_image_times_list(part_1, 4, 1)
+    part_1_1, part_1_2, part_1_3, part_1_4 = (
+        part_1_parts[0],
+        part_1_parts[1],
+        part_1_parts[2],
+        part_1_parts[3],
+    )
+
+    part_1_1_parts = split_image_times_list(part_1_1, 1, 5)
+    (
+        part_1_1_parts_1,
+        part_1_1_parts_2,
+        part_1_1_parts_3,
+        part_1_1_parts_4,
+        part_1_1_parts_5,
+    ) = (
+        part_1_1_parts[0],
+        part_1_1_parts[1],
+        part_1_1_parts[2],
+        part_1_1_parts[3],
+        part_1_1_parts[4],
+    )
+
+
+
+    cv2.imwrite("split/part_1_1_parts_1.jpg", part_1_1_parts_1)
+    cv2.imwrite("split/part_1_1_parts_2.jpg", part_1_1_parts_2)
+    cv2.imwrite("split/part_1_1_parts_3.jpg", part_1_1_parts_3)
+    cv2.imwrite("split/part_1_1_parts_4.jpg", part_1_1_parts_4)
+    cv2.imwrite("split/part_1_1_parts_5.jpg", part_1_1_parts_5)
+    # cv2.imwrite("split/part_1_2.jpg", part_1_2)
+    # cv2.imwrite("split/part_1_3.jpg", part_1_3)
+    # cv2.imwrite("split/part_1_4.jpg", part_1_4)
+
     # Get the contour image for the first contour and its children
-    omr_area_contours_reversed = list(reversed(omr_area_contours))
-    part_1 = get_image_part(omr_area_contours_reversed[2], omr_area)
-    part_2 = get_image_part(omr_area_contours_reversed[4], omr_area)
-    part_3 = get_image_part(omr_area_contours_reversed[6], omr_area)
+    # omr_area_contours_reversed = list(reversed(omr_area_contours))
+    # part_1 = get_image_part(omr_area_contours_reversed[2], omr_area)
+    # part_2 = get_image_part(omr_area_contours_reversed[4], omr_area)
+    # part_3 = get_image_part(omr_area_contours_reversed[6], omr_area)
 
-    
-           
-
-    
     cv2.destroyAllWindows()
 
 
